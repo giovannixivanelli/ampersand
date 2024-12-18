84a49889cebae135cd1294030b09894202a509dc
Ivan Shakin <ivanxshakin1993@gmail.com>
2024-12-18 12:18:26 +0300

    copy button added

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 let button = document.querySelector(".button");
 let input = document.querySelector(".input");
 let output = document.querySelector(".output");
+let copyButton = document.querySelector(".copy-button");
 
-button.addEventListener('click', function (e) {
+button.addEventListener('click', function () {
     let inputValue = input.value;
-    let replacedText = inputValue.replace(/&/g, '&amp;')
+    let replacedText = inputValue.replace(/&/g, '&amp;');
     output.textContent = replacedText;
+});
+
+copyButton.addEventListener('click', function () {
+    navigator.clipboard.writeText(output.value || output.textContent);
 });
